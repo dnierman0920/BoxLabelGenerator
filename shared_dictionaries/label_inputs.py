e0be5b18eee984fcc4058a138fca1222eefdee7f
dnierman0920 <davidnierman93@gmail.com>
2021-11-23 11:41:55 -0800

    Add shared dictionary and label_pdf directories

---
+++ shared_dictionaries/label_inputs.py
@@ -0,0 +1,23 @@
+DEFAULT_LABEL_HEIGHT = 11.5  # inches
+DEFAULT_LABEL_WIDTH = 8.5  # inches
+DEFAULT_NUMBER_OF_LABELS = 1  # page
+
+label_inputs = [
+     'box_length_measured_in',
+     'box_length', 'box_width',
+     'box_height',
+     'box_weight_measured_in',
+     'box_weight',
+     'contains_mixed_skus',
+     'box_full_or_partial',
+     'qty_of_items',
+     'product_item_id',
+     'product_item_upc',
+     'product_item_description',
+     'product_batch',
+     'product_production_date',
+     'product_Expiration_date',
+     'label_height',
+     'label_width',
+     'number_of_labels'
+ ]
